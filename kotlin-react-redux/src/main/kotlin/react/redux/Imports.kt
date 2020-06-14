520e3b67adc66aa55374f6f2f702d48f75718ed4
Walton Hoops <me@waltonhoops.com>
2020-06-13 19:41:01 -0600

    add redux support for React hooks

--- kotlin-react-redux/src/main/kotlin/react/redux/Imports.kt
+++ kotlin-react-redux/src/main/kotlin/react/redux/Imports.kt
@@ -3,7 +3,8 @@
 package react.redux
 
 import react.*
-import redux.*
+import redux.RAction
+import redux.Store
 
 external class Provider : Component<ProviderProps, RState> {
     override fun render(): ReactElement?
@@ -24,3 +25,12 @@ external fun <S, A, R, OP : RProps, P : RProps> connectAdvanced(
     selectorFactory: SelectorFactory<S, A, R, OP, P>,
     options: ConnectOptions<P> = definedExternally
 ): HOC<P, OP>
+
+external fun <S, A, R> useStore(): Store<S, A, R>
+
+external fun useDispatch(): (RAction) -> Unit
+
+external fun <S, T> useSelector(
+    selector: (state: S) -> T,
+    equalityFn: (a: T, b: T) -> Boolean = definedExternally
+): T
